43b91e8b50d8d5b21e87d8261a0153dbe1ab8be3
Bhieu <bhieu332001@gmail.com>
2020-11-06 14:48:44 +0700

    update

---
+++ oop/bomberman/entities/Bomber.java
@@ -0,0 +1,15 @@
+package oop.bomberman.entities;
+
+import java.awt.*;
+import javafx.scene.image.Image;
+
+public class Bomber extends Entity {
+    public Bomber(int x, int y, Image img) {
+        super(x,y,img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
